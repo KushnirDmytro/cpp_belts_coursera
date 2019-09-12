bba54a07cf1058e62ba86e9ddca267d3dd05a6d3
KushnirDmytro <kushnir.dmytro.93@gmail.com>
2019-09-13 01:27:26 +0300

    Group heavy strings added

--- red_belt/w5/group_heavy_strings.cpp
+++ red_belt/w5/group_heavy_strings.cpp
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <list>
+#include <memory>
 
 using namespace std;
 
@@ -20,10 +22,96 @@ using Group = vector<String>;
 template <typename String>
 using Char = typename String::value_type;
 
+class CH{
+    const string v;
+public:
+    CH(string v) : v{v} {
+
+    }
+
+    CH(const CH &rhs) : v{rhs.v}{
+        cout << "Copied el " << v << endl;
+    }
+
+    CH(const CH &&rhs) : v{move(rhs.v)}{
+        cout << "Moved el " << v << endl;
+    }
+
+    CH& operator=(const CH &rhs){
+        *this = CH(rhs.v);
+        cout << "Copied Moved el " << v << endl;
+        return *this;
+    }
+
+    bool operator<(const CH &rhs) const { return v < rhs.v;}
+    bool operator==(const CH &rhs) const { return v == rhs.v;}
+};
+
+template <typename CH>
+class STR{
+    vector<CH> data;
+
+public:
+
+    STR(const STR &&rhs) : data{move(rhs.data)}{
+        cout << "STR moved" << endl;
+    }
+    STR(const STR &rhs) : data{rhs.data}{
+        cout << "STR copied" << endl;
+    }
+
+    bool operator<(const STR &rhs) const { return data < rhs.data;}
+    bool operator==(const STR &rhs)const { return data == rhs.data;}
+
+    typedef CH value_type;
+//    valiue_type = typename CH;
+    STR(vector<CH> data) : data{move(data)} {}
+    auto begin() { return data.begin();}
+    auto end() { return data.end();}
+
+    auto begin() const { return data.begin();}
+    auto end() const { return data.end();}
+};
+
+
+template <typename String>
+map<Char<String>, size_t > MakeAlphabet(vector<String> strings){
+    map<Char<String>, size_t > abet;
+    for(auto &st: strings)
+        for(auto &ch: st){
+            if (abet.find(ch) == abet.end()){
+                abet[move(ch)] = abet.size();
+            }
+        }
+    return abet;
+}
+
+
 template <typename String>
 vector<Group<String>> GroupHeavyStrings(vector<String> strings) {
-  // Напишите реализацию функции,
-  // использовав не более 1 копирования каждого символа
+    using Group = vector<String>;
+
+    auto abet = MakeAlphabet(move(vector<String>{strings}));
+
+    map< set<size_t >, vector<String*>> known_sets;
+
+    for ( String &st : strings){
+        set<size_t> abet_indx_set;
+
+        for( auto &ch: st)
+            abet_indx_set.insert(abet.at(ch));
+        known_sets[abet_indx_set].push_back(&st);
+    }
+
+    vector<Group> res;
+    for (auto &set_to_vec : known_sets){
+        res.emplace_back();
+        for(auto &string_ptr: set_to_vec.second){
+            res.back().push_back(move(*string_ptr));
+        }
+    }
+
+    return res;
 }
 
 
@@ -47,9 +135,23 @@ void TestGroupingReal() {
   ASSERT_EQUAL(groups[3], vector<string>({"top", "pot"}));
 }
 
+void TestGroupingCH() {
+    STR<CH> s1 = {{string("A")}};
+    vector<STR<CH>> strings = { {{string("A")}} };
+    cout << "BEGIN OF ALGO" << endl;
+    auto groups = GroupHeavyStrings(strings);
+    ASSERT_EQUAL(groups.size(), 1);
+    sort(begin(groups), end(groups));  // Порядок групп не имеет значения
+//    ASSERT_EQUAL(groups[0], vector<string>({"laptop", "paloalto"}));
+//    ASSERT_EQUAL(groups[1], vector<string>({"law", "wall", "awl"}));
+//    ASSERT_EQUAL(groups[2], vector<string>({"port"}));
+//    ASSERT_EQUAL(groups[3], vector<string>({"top", "pot"}));
+}
+
 int main() {
   TestRunner tr;
   RUN_TEST(tr, TestGroupingABC);
   RUN_TEST(tr, TestGroupingReal);
+//  RUN_TEST(tr, TestGroupingCH);
   return 0;
 }
