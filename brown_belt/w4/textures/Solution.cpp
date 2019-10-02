db5767d17132875fe83da9a7bfd045c3308bff59
KushnirDmytro <kushnir.dmytro.93@gmail.com>
2019-10-02 14:59:48 +0300

    Test cases passed, Issue with err corrupted size vs. prev_size

--- brown_belt/w4/textures/Solution.cpp
+++ brown_belt/w4/textures/Solution.cpp
@@ -2,57 +2,108 @@
 #include "Textures.h"
 using namespace std;
 
-// Этот файл сдаётся на проверку
-// Здесь напишите реализацию необходимых классов-потомков `IShape`
+Size operator&(const Size& lhs, const Size &rhs){
+    return {
+            min(lhs.width, rhs.width),
+            min(lhs.height, rhs.height)
+    };
+}
+
+class SingletonNullTexture : public ITexture {
+public:
+
+     Size GetSize() const override {
+        static const Size empty_size_{0,0};
+        return empty_size_;
+    }
+
+    const Image& GetImage() const override {
+        static const Image null_image_{};
+        return null_image_;
+    }
+};
+
+class SingletonNullTextureManager {
+public:
+    static ITexture* GetSingletonNullTexturePtr(){
+        static const unique_ptr<SingletonNullTexture> singletonNullTexture =
+                make_unique<SingletonNullTexture>();
+        return singletonNullTexture.get();
+    }
+};
+
+class AbstractFigure;
+void DrawFigure(const AbstractFigure *fig, Image& canvas_image);
+
 
 class AbstractFigure : public IShape{
 public:
-     void SetPosition(Point new_position) override { position_ = new_position; }
-     Point GetPosition() const override { return position_; }
+     void SetPosition(Point new_position) final { position_ = new_position; }
+     Point GetPosition() const final { return position_; }
 
-     void SetSize(Size new_size) override { size_ = new_size; }
-     Size GetSize() const override { return size_; }
+     void SetSize(Size new_size) final { size_ = new_size; }
+     Size GetSize() const final { return size_; }
 
-     void SetTexture(std::shared_ptr<ITexture> new_texture) { texture_ = new_texture; }
-     ITexture* GetTexture() const { return texture_.get(); }
+     void SetTexture(std::shared_ptr<ITexture> new_texture) final { texture_ = new_texture; }
+     ITexture* GetTexture() const final {
+         return texture_ ?
+                texture_.get() :
+                SingletonNullTextureManager::GetSingletonNullTexturePtr();
+     }
 
+     tuple<Size, Size, Size> GetOverlapingRegionsWith(const Image& canvas_image) const {
+         Size sh_tx_overlap = GetTexture()->GetSize();
+
+         Size canvas_image_size{static_cast<int>(canvas_image[0].size()),
+                                static_cast<int>(canvas_image.size())};
+
+         Size im_tx_overlap = sh_tx_overlap & canvas_image_size;
+         Size sh_im_overlap = canvas_image_size & size_;
+         return {sh_tx_overlap, im_tx_overlap, sh_im_overlap};
+     }
+
+    inline bool isTextured(const Point& p) const { // presume that it allways belong to img
+        const Size& ts = GetTexture()->GetSize();
+        return p.x < ts.width && p.y < ts.height;
+    }
+
+     virtual bool isShapeCovered(const Point&) const = 0;
+    void Draw(Image& canvas_image) const final {
+        DrawFigure(this, canvas_image);
+    }
 protected:
     Size size_;
     Point position_;
-    shared_ptr<ITexture> texture_ = make_shared<ITexture>();
+    shared_ptr<ITexture> texture_{nullptr};
 };
 
-Size operator&(const Size& lhs, const Size &rhs){
-    return {
-        min(lhs.width, rhs.width),
-        min(lhs.height, rhs.height)
-    };
+
+void DrawFigure(const AbstractFigure *fig, Image& canvas_image){
+    Size sh_tx_overlap, im_tx_overlap, sh_im_overlap;
+    tie(sh_tx_overlap, im_tx_overlap, sh_im_overlap) =
+            fig->GetOverlapingRegionsWith(canvas_image);
+
+    const auto figure_texture = fig->GetTexture();
+    const Image &texture_im = figure_texture->GetImage();
+
+    Point shape_origin = fig->GetPosition();
+
+    Point p{0, 0};
+    for (; p.y < sh_im_overlap.height; ++p.y,  p.x = 0){
+        for (; p.x < sh_im_overlap.width; ++p.x){
+            if (fig->isShapeCovered(p))
+            canvas_image[p.y + shape_origin.y][p.x + shape_origin.x] = \
+                fig->isTextured(p)
+                ? texture_im[p.y][p.x] \
+                : '.';
+        }
+    }
 }
 
 
 class Rectangle : public  AbstractFigure{
-    // Возвращает точную копию фигуры.
-    // Если фигура содержит текстуру, то созданная копия содержит ту же самую
-    // текстуру. Фигура и её копия совместно владеют этой текстурой.
 
-    void Draw(Image& canvas_image) const {
-        Size sh_tx_overlap = texture_->GetSize() & size_;
-        Size canvas_image_size{static_cast<int>(canvas_image[0].size()),
-                               static_cast<int>(canvas_image.size())};
-        Size im_tx_overlap = sh_tx_overlap & canvas_image_size;
-        Size sh_im_overlap = canvas_image_size & size_;
-
-        const Image &texture_im = texture_->GetImage();
-        for (int h{0}; h < sh_im_overlap.height; ++h){
-            for (int w{0}; h < sh_im_overlap.width; ++w){
-                canvas_image[h][w] = \
-                (w < im_tx_overlap.width && h < im_tx_overlap.height) \
-                ? canvas_image[h][w] = texture_im[h][w] \
-                : canvas_image[h][w] = '.';
-            }
-        }
-    }
-
+    inline bool isShapeCovered(const Point&) const override { return true;}
 
     std::unique_ptr<IShape> Clone() const override {
         return make_unique<Rectangle>(Rectangle(*this));
@@ -60,35 +111,15 @@ class Rectangle : public  AbstractFigure{
 };
 
 class Ellipse : public AbstractFigure {
-    // Возвращает точную копию фигуры.
-    // Если фигура содержит текстуру, то созданная копия содержит ту же самую
-    // текстуру. Фигура и её копия совместно владеют этой текстурой.
-    void Draw(Image& canvas_image) const {
 
-        Size sh_tx_overlap = texture_->GetSize() & size_;
-        Size canvas_image_size{static_cast<int>(canvas_image[0].size()),
-                               static_cast<int>(canvas_image.size())};
-        Size im_tx_overlap = sh_tx_overlap & canvas_image_size;
-        Size sh_im_overlap = canvas_image_size & size_;
-
-        const Image &texture_im = texture_->GetImage();
-        for (int h{0}; h < sh_im_overlap.height; ++h){
-            for (int w{0}; h < sh_im_overlap.width; ++w){
-                if (IsPointInEllipse({w, h}, size_))
-                canvas_image[h][w] = \
-                (w < im_tx_overlap.width && h < im_tx_overlap.height) \
-                ? canvas_image[h][w] = texture_im[h][w] \
-                : canvas_image[h][w] = '.';
-            }
-        }
+    inline bool isShapeCovered(const Point& p) const override{
+        return IsPointInEllipse(p, GetSize());
     }
-
 
     std::unique_ptr<IShape> Clone() const override {
         return make_unique<Ellipse>(Ellipse(*this));
     }
 };
-
 
 // Напишите реализацию функции
 unique_ptr<IShape> MakeShape(ShapeType shape_type) {
@@ -97,5 +128,7 @@ unique_ptr<IShape> MakeShape(ShapeType shape_type) {
             return make_unique<Rectangle>(Rectangle());
         case ShapeType::Ellipse:
             return make_unique<Ellipse>(Ellipse());
+        default:
+            return nullptr; // make compiler happy
     }
 }
